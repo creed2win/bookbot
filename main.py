a2de4cb53dbc5ffe0b8e19c7f6384948b5cb01ee
creed2win <michal.dovole@gmail.com>
2024-08-13 08:22:44 +0200

    how to sort dictionary

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ def main():
         file_contents = f.read()
         print(file_contents)
         print("Number of words in text: " + str(word_count(file_contents)))
+        print(char_count(file_contents))
 
 def word_count(text):
     words = text.split()
     word_count = len(words)
     return word_count
+
+def char_count(text):
+    lower_text = text.lower()
+
+    char_count = {}
+    for char in lower_text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 
 main()
